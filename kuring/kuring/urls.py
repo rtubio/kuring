840dcaa6c1a4ccce149f8b029b7a6231f81c2cf8
Ricardo Tubio-Pardavila <rtpardavila@gmail.com>
2020-02-07 14:15:29 +0900

    (backup)

--- kuring/kuring/urls.py
+++ kuring/kuring/urls.py
@@ -19,6 +19,6 @@ from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name = 'dashboard.html'), name='index'),
+    path('', TemplateView.as_view(template_name = 'dashboard.html'), name='dashboard'),
     path('admin/', admin.site.urls),
 ]
