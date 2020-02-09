ff74ad22380ee5332853e81203e12d775cebe38a
Ricardo Tubio-Pardavila <rtpardavila@gmail.com>
2020-02-09 14:13:54 +0900

    Environment configuration improved

--- kuring/kuring/settings/common.py
+++ kuring/kuring/settings/common.py
@@ -1,4 +1,4 @@
-
+import importlib.util
 import json
 import os
 import sys
@@ -7,169 +7,203 @@ import sys
 """Django COMMON settings for kuring project."""
 
 
-def printConfiguration(mode, debug, loglevel, hosts):
-    """This function prints a basic configuration status message."""
-    print(f"> Executing in {mode} mode: debug ({debug}), hosts ({hosts}), loglevel ({loglevel})")
-    print(f"> BASE_DIR ({BASE_DIR}), STATIC_ROOT ({STATIC_ROOT})")
+def configurationConstructor(module, mode, debug, hosts, databaseConfig, logLevel):
+
+    builder = ConfigurationBuilder(mode, debug, hosts, databaseConfig, logLevel)
+
+    for k in builder.config:
+        setattr(module, k, builder.config[k])
 
 
-def configureBasicLogger(loglevel):
-    """This function creates a basic logging object."""
+class ConfigurationBuilder():
 
-    return {
-        'version': 1,
-        'disable_existing_loggers': False,
-        'filters': {
-            'require_debug_false': {
-                '()': 'django.utils.log.RequireDebugFalse'
-            }
-        },
-        'formatters': {
-            'verbose': {
-                'format': '%(asctime)s %(levelname)s [%(name)s:%(lineno)s] %(module)s %(process)d %(thread)d %(message)s'
-            }
-        },
-        'handlers': {
-            'console': {
-                'level': loglevel,
-                'class': 'logging.StreamHandler',
-                'stream': sys.stdout,
-                'formatter': 'verbose'
-            }
-        },
-        'loggers': {
-            'django':{
-                'level': loglevel,
-                'handlers': ['console'],
-                'propagate': True,
+
+    def __init__(self, mode, debug, hosts, databaseConfig, logLevel):
+        """Basic constructor"""
+        self.mode = mode
+        self.debug = debug
+        self.hosts = hosts
+        self.dbconfig = databaseConfig
+        self.loglevel = logLevel
+        self.config = {}
+
+        self.buildConfig()
+
+
+    def print(self):
+        """This function prints a basic configuration status message."""
+        print(f"> mode: {self.mode}, debug ({self.debug}), hosts ({self.hosts}), loglevel ({self.loglevel})")
+        print(f"> BASE_DIR ({BASE_DIR}), STATIC_ROOT ({STATIC_ROOT})")
+
+
+    def logger(self):
+        """This function creates a basic logging object."""
+
+        return {
+            'version': 1,
+            'disable_existing_loggers': False,
+            'filters': {
+                'require_debug_false': {
+                    '()': 'django.utils.log.RequireDebugFalse'
+                }
             },
-           'gunicorn.errors': {
-                'level': loglevel,
-                'handlers': ['console'],
-                'propagate': True,
-            }
-        },
-    }
+            'formatters': {
+                'verbose': {
+                    'format': '%(asctime)s %(levelname)s [%(name)s:%(lineno)s] %(module)s %(process)d %(thread)d %(message)s'
+                }
+            },
+            'handlers': {
+                'console': {
+                    'level': self.loglevel,
+                    'class': 'logging.StreamHandler',
+                    'stream': sys.stdout,
+                    'formatter': 'verbose'
+                }
+            },
+            'loggers': {
+                'django':{
+                    'level': self.loglevel,
+                    'handlers': ['console'],
+                    'propagate': True,
+                },
+               'gunicorn.errors': {
+                    'level': self.loglevel,
+                    'handlers': ['console'],
+                    'propagate': True,
+                }
+            },
+        }
 
 
-def loaddb(dbconfig):
-    """This function loads the configuration for the database from the given file."""
+    def database(self):
+        """This function loads the configuration for the database from the given file."""
 
-    with open(dbconfig) as file:
-        db_secrets = json.load(file)
+        with open(self.dbconfig) as file:
+            db_secrets = json.load(file)
 
-    return {
-        'default': {
-            'ENGINE': db_secrets['ngin'],
-            'NAME': db_secrets['name'],
-            'USER': db_secrets['user'],
-            'PASSWORD': db_secrets['pass'],
-            'HOST': db_secrets['host'],
-            'PORT': db_secrets['port'],
-            'TEST': {
-                'CHARSET': 'utf8',
-                'COLLATION': 'utf8_general_ci',
+        self.config['DATABASES'] = {
+            'default': {
+                'ENGINE': db_secrets['ngin'],
+                'NAME': db_secrets['name'],
+                'USER': db_secrets['user'],
+                'PASSWORD': db_secrets['pass'],
+                'HOST': db_secrets['host'],
+                'PORT': db_secrets['port'],
+                'TEST': {
+                    'CHARSET': 'utf8',
+                    'COLLATION': 'utf8_general_ci',
+                }
             }
         }
-    }
 
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(os.path.join(__file__, '../.'))))
+    def celery(self, config):
+        """This function loads the configuration for Celery from a given file"""
+        # NOTE # for backend configuraiton: # 'redis://localhost:6379/0' # 'django-db'
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
-STATIC_URL = '/static/'
+        # ### CELERY configuration
+        with open(config) as file:
+            celery_cfg = json.load(file)
 
-# Routing
-ROOT_URLCONF = 'kuring.urls'
-WSGI_APPLICATION = 'kuring.wsgi.application'
-ASGI_APPLICATION = 'kuring.routing.application'
+        self.config['CELERY_BROKER_URL'] = celery_cfg['broker']
+        self.config['CELERY_RESULT_BACKEND'] = celery_cfg['backend']
+        self.config['CELERY_CACHE_BACKEND'] = celery_cfg['backend-cache']
+        self.config['CELERY_ACCEPT_CONTENT'] = celery_cfg['accept-content']
+        self.config['CELERY_TASK_SERIALIZER'] = celery_cfg['task-serializer']
+        self.config['CELERY_RESULT_SERIALIZER'] = celery_cfg['result-serializer']
 
-# Site ID, see configuration for 'django.contrib.sites'
-SITE_ID = 1
 
-# Internationalization
-# https://docs.djangoproject.com/en/3.0/topics/i18n/
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
-USE_I18N = True
-USE_L10N = True
-USE_TZ = True
+    def buildConfig(self):
 
-# SECURITY WARNING: keep the secret key used in production secret!
-with open('../.__skr__/django.json') as file:
-    django_secrets = json.load(file)
-SECRET_KEY = django_secrets['djsk']
+        # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+        self.config['BASE_DIR'] = os.path.dirname(os.path.dirname(os.path.abspath(os.path.join(__file__, '../.'))))
 
-# Application definition
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.sites',
-    'django.contrib.staticfiles',
-    ### Dependencies
-    'django_celery_results',
-    'bootstrap4',
-    'django_icons',
-    'channels',
-    ### 自分で
-    'tasker'
-]
+        # Static files (CSS, JavaScript, Images)
+        # https://docs.djangoproject.com/en/3.0/howto/static-files/
+        self.config['STATIC_ROOT'] = os.path.abspath(os.path.join(self.config['BASE_DIR'], '../static'))
+        self.config['STATIC_URL'] = '/static/'
 
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
+        # Routing
+        self.config['ROOT_URLCONF'] = 'kuring.urls'
+        self.config['WSGI_APPLICATION'] = 'kuring.wsgi.application'
+        self.config['ASGI_APPLICATION'] = 'kuring.routing.application'
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS':  [os.path.join(BASE_DIR, 'kuring', 'templates')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
+        # Site ID, see configuration for 'django.contrib.sites'
+        self.config['SITE_ID'] = 1
 
-# Password validation
-# https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
-AUTH_PASSWORD_VALIDATORS = [
-    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',},
-    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',},
-    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',},
-    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',},
-]
+        # Internationalization
+        # https://docs.djangoproject.com/en/3.0/topics/i18n/
+        self.config['LANGUAGE_CODE'] = 'en-us'
+        self.config['TIME_ZONE'] = 'UTC'
+        self.config['USE_I18N'] = True
+        self.config['USE_L10N'] = True
+        self.config['USE_TZ'] = True
 
-# Extra static file directories, outside of the common 'static' for all apps
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "kuring", "static"),
-]
+        # SECURITY WARNING: keep the secret key used in production secret!
+        with open('../.__skr__/django.json') as file:
+            django_secrets = json.load(file)
+        self.config['SECRET_KEY'] = django_secrets['djsk']
 
-# ### CELERY configuration
-with open('../config/celery.json') as file:
-    celery_cfg = json.load(file)
-CELERY_BROKER_URL = celery_cfg['broker']
-CELERY_RESULT_BACKEND = celery_cfg['backend']   # 'redis://localhost:6379/0' # 'django-db'
-CELERY_CACHE_BACKEND = celery_cfg['backend-cache']
+        # Application definition
+        self.config['INSTALLED_APPS'] = [
+            'django.contrib.admin',
+            'django.contrib.auth',
+            'django.contrib.contenttypes',
+            'django.contrib.sessions',
+            'django.contrib.messages',
+            'django.contrib.sites',
+            'django.contrib.staticfiles',
+            ### Dependencies
+            'django_celery_results',
+            'bootstrap4',
+            'django_icons',
+            'channels',
+            ### 自分で
+            'tasker'
+        ]
 
-# TODO # check why it does not work
-# CELERY_ACCEPT_CONTENT = celery_cfg['application/json']
-# CELERY_TASK_SERIALIZER = celery_cfg['json']
-# CELERY_RESULT_SERIALIZER = celery_cfg['json']
+        self.config['MIDDLEWARE'] = [
+            'django.middleware.security.SecurityMiddleware',
+            'django.contrib.sessions.middleware.SessionMiddleware',
+            'django.middleware.common.CommonMiddleware',
+            'django.middleware.csrf.CsrfViewMiddleware',
+            'django.contrib.auth.middleware.AuthenticationMiddleware',
+            'django.contrib.messages.middleware.MessageMiddleware',
+            'django.middleware.clickjacking.XFrameOptionsMiddleware',
+        ]
+
+        self.config['TEMPLATES'] = [
+            {
+                'BACKEND': 'django.template.backends.django.DjangoTemplates',
+                'DIRS':  [os.path.join(self.config['BASE_DIR'], 'kuring', 'templates')],
+                'APP_DIRS': True,
+                'OPTIONS': {
+                    'context_processors': [
+                        'django.template.context_processors.debug',
+                        'django.template.context_processors.request',
+                        'django.contrib.auth.context_processors.auth',
+                        'django.contrib.messages.context_processors.messages',
+                    ],
+                },
+            },
+        ]
+
+        # Password validation
+        # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
+        self.config['AUTH_PASSWORD_VALIDATORS'] = [
+            {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',},
+            {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',},
+            {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',},
+            {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',},
+        ]
+
+        # Extra static file directories, outside of the common 'static' for all apps
+        # https://docs.djangoproject.com/en/3.0/howto/static-files/
+        self.config['STATICFILES_DIRS'] = [
+            os.path.join(self.config['BASE_DIR'], "kuring", "static"),
+        ]
+
+        self.config['LOGGING'] = self.logger()
+
+        self.database()
+        self.celery('../config/celery.json')
--- kuring/kuring/settings/production.py
+++ kuring/kuring/settings/production.py
@@ -1,6 +1,4 @@
 
-import json
-import os
 import sys
 import kuring.settings.common as settings
 
@@ -10,37 +8,10 @@ ALLOWED_HOSTS = []
 DBCONFIG = '../.__skr__/db.json'
 DEBUG = True
 LOG_LEVEL = 'INFO'
-MODE = str(__file__).split('.')[0].split('/')[-1]
+MODE = 'Production Mode'
 
-settings.printConfiguration(MODE, DEBUG, LOG_LEVEL, ALLOWED_HOSTS)
-
-BASE_DIR = settings.BASE_DIR
-SITE_ID = settings.SITE_ID
-SECRET_KEY = settings.SECRET_KEY
-INSTALLED_APPS = settings.INSTALLED_APPS
-MIDDLEWARE = settings.MIDDLEWARE
-ROOT_URLCONF = settings.ROOT_URLCONF
-TEMPLATES = settings.TEMPLATES
-WSGI_APPLICATION = settings.WSGI_APPLICATION
-ASGI_APPLICATION = settings.ASGI_APPLICATION
-AUTH_PASSWORD_VALIDATORS = settings.AUTH_PASSWORD_VALIDATORS
-
-LANGUAGE_CODE = settings.LANGUAGE_CODE
-TIME_ZONE = settings.TIME_ZONE
-USE_I18N = settings.USE_I18N
-USE_L10N = settings.USE_L10N
-USE_TZ = settings.USE_TZ
-
-STATIC_URL = settings.STATIC_URL
-STATIC_ROOT = settings.STATIC_ROOT
-STATICFILES_DIRS = settings.STATICFILES_DIRS
-
-CELERY_BROKER_URL = settings.CELERY_BROKER_URL
-CELERY_RESULT_BACKEND = settings.CELERY_RESULT_BACKEND
-CELERY_CACHE_BACKEND = settings.CELERY_CACHE_BACKEND
-# CELERY_ACCEPT_CONTENT = settings.CELERY_ACCEPT_CONTENT
-# CELERY_TASK_SERIALIZER = settings.CELERY_TASK_SERIALIZER
-# CELERY_RESULT_SERIALIZER = settings.CELERY_RESULT_SERIALIZER
-
-DATABASES = settings.loaddb(DBCONFIG)
-LOGGING = settings.configureBasicLogger(LOG_LEVEL)
+settings.configurationConstructor(
+    sys.modules[__name__],
+    str(__file__).split('.')[0].split('/')[-1],
+    DEBUG, ALLOWED_HOSTS, DBCONFIG, LOG_LEVEL
+)
