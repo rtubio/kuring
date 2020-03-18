ba7db5c6d6d5308ec4b51d53b22264fc7b5f4552
Ricardo Tubio-Pardavila <rtpardavila@gmail.com>
2020-03-18 17:47:31 +0900

    Added MLX90614 IR contactless sensor

--- kuring/drivers/ivsystem/ivscreen.ino
+++ kuring/drivers/ivsystem/ivscreen.ino
@@ -2,6 +2,7 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <Adafruit_INA219.h>
+#include <Adafruit_MLX90614.h>
 
 #define SERIAL_SPEED    9600          // Serial port speed
 #define SERIAL_WAIT     100           // Serial port readiness waiting checkpoint
@@ -29,10 +30,12 @@ const float ACS721_FACTOR   = ( ADC_RANGE * ACS712_MVAMP ) / ( ACS712_SAMPLES * 
 
 Adafruit_SSD1306 display(128, 64);
 Adafruit_INA219 ina219(I2C_INA219_ADDR);
+Adafruit_MLX90614 mlx90614 = Adafruit_MLX90614();
 
 
-float acs721_current = 0.0;
+// float acs721_current = 0.0;
 float shuntVoltage_mV = 0.0, busVoltage = 0.0, loadCurrent_mA = 0.0, loadVoltage = 0.0;
+double ambienceT_degC = 0.0, objectT_degC = 0.0;
 
 
 void scanI2C() {
@@ -56,7 +59,7 @@ void scanI2C() {
 }
 
 
-void setup_I2CLCD(){
+void setupLCD() {
   delay(I2C_LCD_WAIT);
   display.begin(SSD1306_SWITCHCAPVCC, I2C_LCD_ADDR);
   display.clearDisplay();
@@ -68,14 +71,27 @@ void setup_I2CLCD(){
 }
 
 
-void ACS721_measure() {
+void setupINA219() {
+  ina219.begin();
+  ina219.setCalibration_32V_1A();
+}
+
+
+void setupMLX90614() {
+  mlx90614.begin();
+}
+
+
+/*
+void measure_ACS721() {
   int buffer = 0, read = 0;
   for (byte i = 0; i < ACS712_SAMPLES; i++) { buffer += analogRead(A1); delay(ACS712_WAIT_SAMPLE); }
   acs721_current = buffer * ACS721_FACTOR;
 }
+*/
 
 
-void INA219_measure() {
+void measureINA219() {
   shuntVoltage_mV = ina219.getShuntVoltage_mV();
   busVoltage      = ina219.getBusVoltage_V();
   loadCurrent_mA  = ina219.getCurrent_mA();
@@ -83,46 +99,61 @@ void INA219_measure() {
 }
 
 
+void measureMLX90614() {
+ ambienceT_degC = mlx90614.readAmbientTempC();
+ objectT_degC   = mlx90614.readObjectTempC();
+}
+
+
 void updateDisplay() {
   display.clearDisplay();
   display.setCursor(0, 5);
   display.println("KarbonTek - IV Tracer");
-  display.println("");
+
+  // display.println("");
   display.println(String("V (bus ,  V): ") + busVoltage);
   display.println(String("V (shnt, mV): ") + shuntVoltage_mV);
   display.println(String("V (load,  V): ") + loadVoltage);
   display.println(String("I (load, mA): ") + loadCurrent_mA);
-  display.println(String("7 (load, mA): ") + acs721_current);
+  // display.println(String("7 (load, mA): ") + acs721_current);
+  display.println(String("Ta(C) = ") + ambienceT_degC);
+  display.println(String("To(C) = ") + objectT_degC);
+
   display.display();
 }
 
 
-void setup() {
-
+void setupSerial() {
   Serial.setTimeout(SERIAL_TIMEOUT);
   Serial.begin(SERIAL_SPEED);
   while (!Serial) { delay(SERIAL_WAIT); }
   Serial.println(String("Serial port ready, working at (bps) = ") + SERIAL_SPEED);
+}
 
+
+void setupI2C() {
   // Find I2C devices, only to be executed once to find the addresses of the devices
   // This function should not be used dynamically, since the addresses for the devices should be static
-  // for (int i = 0; i < I2C_MAX_SLAVES; i++) { devices[i] = false; }
-  // scanI2C();
+  for (int i = 0; i < I2C_MAX_SLAVES; i++) { devices[i] = false; }
+  scanI2C();
+}
 
-  setup_I2CLCD();
+void setup() {
 
-  ina219.begin();
-  ina219.setCalibration_32V_1A();
+  setupSerial();
+  setupI2C();
+  setupLCD();
+  setupINA219();
+  setupMLX90614();
 
 }
 
 
 void loop() {
 
-  Serial.println("Measuring");
-  INA219_measure();
-  // ACS721_measure();
-  Serial.println(String("> ACS721, I = ") + acs721_current);
+  Serial.print("*");
+  measureINA219();
+  measureMLX90614();
   updateDisplay();
   delay(PAUSE);
 
